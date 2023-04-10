ffa08a9702e55d5c6a8888251e947a5fe7b6bb59
nvillalonga <nvillalonga@udesa.edu.ar>
2023-04-10 10:18:31 -0300

    Agregado de nombres y fecha de carga

--- db/autos.js
+++ db/autos.js
@@ -4,20 +4,20 @@ let autos = {
             nombre: 'Audi A4',
             imagen: '/images/productos/Audi_A4.jpeg',
             descripcion: '',
-            fecha_de_carga: '',
+            fecha_de_carga: '01/01/2022',
             comentarios: [
                 {
-                    nombre_usuario: '',
+                    nombre_usuario: 'Nicolas',
                     foto_perfil_usuario: '',
                     comentario: ''
                 },
                 {
-                    nombre_usuario: '',
+                    nombre_usuario: 'Joaquin',
                     foto_perfil_usuario: '',
                     comentario: ''
                 },
                 {
-                    nombre_usuario: '',
+                    nombre_usuario: 'Lucio',
                     foto_perfil_usuario: '',
                     comentario: ''
                 },
@@ -27,20 +27,20 @@ let autos = {
             nombre: 'Audi A5 Coupe',
             imagen: '/images/productos/Audi_A5_Coupe.jpeg',
             descripcion: '',
-            fecha_de_carga: '',
+            fecha_de_carga: '01/02/2022',
             comentarios: [
                 {
-                    nombre_usuario: '',
+                    nombre_usuario: 'Felipe',
                     foto_perfil_usuario: '',
                     comentario: ''
                 },
                 {
-                    nombre_usuario: '',
+                    nombre_usuario: 'Valentin',
                     foto_perfil_usuario: '',
                     comentario: ''
                 },
                 {
-                    nombre_usuario: '',
+                    nombre_usuario: 'Santiago',
                     foto_perfil_usuario: '',
                     comentario: ''
                 },
@@ -49,20 +49,20 @@ let autos = {
             nombre: 'Audi A6 Allroad',
             imagen: '/images/productos/Audi_A6_Allroad.jpeg',
             descripcion: '',
-            fecha_de_carga: '',
+            fecha_de_carga: '01/03/2022',
             comentarios: [
                 {
-                    nombre_usuario: '',
+                    nombre_usuario: 'Felix',
                     foto_perfil_usuario: '',
                     comentario: ''
                 },
                 {
-                    nombre_usuario: '',
+                    nombre_usuario: 'Santiago',
                     foto_perfil_usuario: '',
                     comentario: ''
                 },
                 {
-                    nombre_usuario: '',
+                    nombre_usuario: 'Benjamin',
                     foto_perfil_usuario: '',
                     comentario: ''
                 },
@@ -71,20 +71,20 @@ let autos = {
             nombre: 'Audi A7 Sportpack',
             imagen: '/images/productos/Audi_A7_Sportpack.jpeg',
             descripcion: '',
-            fecha_de_carga: '',
+            fecha_de_carga: '01/04/2022',
             comentarios: [
                 {
-                    nombre_usuario: '',
+                    nombre_usuario: 'Justo',
                     foto_perfil_usuario: '',
                     comentario: ''
                 },
                 {
-                    nombre_usuario: '',
+                    nombre_usuario: 'Geronimo',
                     foto_perfil_usuario: '',
                     comentario: ''
                 },
                 {
-                    nombre_usuario: '',
+                    nombre_usuario: 'Santino',
                     foto_perfil_usuario: '',
                     comentario: ''
                 },
@@ -93,20 +93,20 @@ let autos = {
             nombre: 'Audi A8',
             imagen: '/images/productos/Audi_A8.jpeg',
             descripcion: '',
-            fecha_de_carga: '',
+            fecha_de_carga: '01/05/2022',
             comentarios: [
                 {
-                    nombre_usuario: '',
+                    nombre_usuario: 'Julieta',
                     foto_perfil_usuario: '',
                     comentario: ''
                 },
                 {
-                    nombre_usuario: '',
+                    nombre_usuario: 'Pedro',
                     foto_perfil_usuario: '',
                     comentario: ''
                 },
                 {
-                    nombre_usuario: '',
+                    nombre_usuario: 'Lucia',
                     foto_perfil_usuario: '',
                     comentario: ''
                 },
@@ -115,20 +115,20 @@ let autos = {
             nombre: 'Audi e-tron GT',
             imagen: '/images/productos/Audi_e-tron_GT.jpeg',
             descripcion: '',
-            fecha_de_carga: '',
+            fecha_de_carga: '01/06/2022',
             comentarios: [
                 {
-                    nombre_usuario: '',
+                    nombre_usuario: 'Juana',
                     foto_perfil_usuario: '',
                     comentario: ''
                 },
                 {
-                    nombre_usuario: '',
+                    nombre_usuario: 'Brian',
                     foto_perfil_usuario: '',
                     comentario: ''
                 },
                 {
-                    nombre_usuario: '',
+                    nombre_usuario: 'Miguel',
                     foto_perfil_usuario: '',
                     comentario: ''
                 },
@@ -137,20 +137,20 @@ let autos = {
             nombre: 'Audi Q2',
             imagen: '/images/productos/Audi_Q2.jpeg',
             descripcion: '',
-            fecha_de_carga: '',
+            fecha_de_carga: '01/07/2022',
             comentarios: [
                 {
-                    nombre_usuario: '',
+                    nombre_usuario: 'Gonzalo',
                     foto_perfil_usuario: '',
                     comentario: ''
                 },
                 {
-                    nombre_usuario: '',
+                    nombre_usuario: 'Santos',
                     foto_perfil_usuario: '',
                     comentario: ''
                 },
                 {
-                    nombre_usuario: '',
+                    nombre_usuario: 'Milagros',
                     foto_perfil_usuario: '',
                     comentario: ''
                 },
@@ -159,20 +159,20 @@ let autos = {
             nombre: 'Audi Q3',
             imagen: '/images/productos/Audi_Q3.jpeg',
             descripcion: '',
-            fecha_de_carga: '',
+            fecha_de_carga: '01/08/2022',
             comentarios: [
                 {
-                    nombre_usuario: '',
+                    nombre_usuario: 'Pilar',
                     foto_perfil_usuario: '',
                     comentario: ''
                 },
                 {
-                    nombre_usuario: '',
+                    nombre_usuario: 'Gaston',
                     foto_perfil_usuario: '',
                     comentario: ''
                 },
                 {
-                    nombre_usuario: '',
+                    nombre_usuario: 'Marcela',
                     foto_perfil_usuario: '',
                     comentario: ''
                 },
@@ -181,20 +181,20 @@ let autos = {
             nombre: 'Audi Q5',
             imagen: '/images/productos/Audi_Q5.jpeg',
             descripcion: '',
-            fecha_de_carga: '',
+            fecha_de_carga: '01/09/2022',
             comentarios: [
                 {
-                    nombre_usuario: '',
+                    nombre_usuario: 'Ricardo',
                     foto_perfil_usuario: '',
                     comentario: ''
                 },
                 {
-                    nombre_usuario: '',
+                    nombre_usuario: 'Emiliano',
                     foto_perfil_usuario: '',
                     comentario: ''
                 },
                 {
-                    nombre_usuario: '',
+                    nombre_usuario: 'Tomas',
                     foto_perfil_usuario: '',
                     comentario: ''
                 },
@@ -203,20 +203,20 @@ let autos = {
             nombre: 'Audi Q7',
             imagen: '/images/productos/Audi_Q7.jpeg',
             descripcion: '',
-            fecha_de_carga: '',
+            fecha_de_carga: '01/10/2022',
             comentarios: [
                 {
-                    nombre_usuario: '',
+                    nombre_usuario: 'Manuel',
                     foto_perfil_usuario: '',
                     comentario: ''
                 },
                 {
-                    nombre_usuario: '',
+                    nombre_usuario: 'Sofia',
                     foto_perfil_usuario: '',
                     comentario: ''
                 },
                 {
-                    nombre_usuario: '',
+                    nombre_usuario: 'Francesca',
                     foto_perfil_usuario: '',
                     comentario: ''
                 },
