3b633d26b788e457b8908d79023601d8825844ce
nvillalonga <nvillalonga@udesa.edu.ar>
2023-04-10 18:33:46 -0300

    cambios en data

--- data/data.js
+++ data/data.js
@@ -3,53 +3,63 @@ const data = {
         {
             nombre: 'Audi A4',
             imagen: '/images/productos/Audi_A4.jpeg',
-            descripcion: 'El sedán más chico de Audi (antes de la llegada del A3 con baúl) sigue su marcha con esta nueva generación que creció mucho respecto de la anterior. Se destaca el espacio interior, la calidad de la cambina y elementos como el cuadro de instrumentos 100% digital. Si bien el nuevo 2.0 TSi no entrega toda la potencia esperada, esto se debe a su ciclo de funcionamiento pseudo Miller que le permite consumos destacados. Si lo que buscás es potencia, entonces hay un 2.0 TSi de 252 caballos y un 3.0L V6 con 100 CV más.',
+            descripcion: 'Una berlina premium, del segmento D.',
             fecha_de_carga: '01/01/2022',
         },{
             nombre: 'Audi A5 Coupe',
             imagen: '/images/productos/Audi_A5_Coupe.jpeg',
-            descripcion: 'Hermano dos puertas del A4, el A5 entró en su segunda generación con un estilo más trabajado, interior soberbio y los mejores trucos de la marca incluyendo motores turbo y tracción integral quattro.',
+            descripcion: 'Una berlina coupé, del segmento D.',
             fecha_de_carga: '01/02/2022',
         },{
             nombre: 'Audi A6 Allroad',
             imagen: '/images/productos/Audi_A6_Allroad.jpeg',
-            descripcion: 'Antes de tener su gama Q de SUVs, Audi estreno el mundo de la aventura con el A6 Allroad, una Avant preparada con protectores, tracción quattro y hasta suspensión neumática con despeje variable. Hoy latradición sigue, combiando confort, capacidad y manejo en un combo que promete volver a ponmerse de moda.',
+            descripcion: 'Es la variante todocamino del Audi A6 Avant.',
             fecha_de_carga: '01/03/2022',
         },{
             nombre: 'Audi A7 Sportpack',
             imagen: '/images/productos/Audi_A7_Sportpack.jpeg',
-            descripcion: 'El A7 es la encarnación deportiva del lujo según Audi con una silueta de cuatro puertas cruzada con el estilo de una coupé en su pronunciada caida del techo al estilo fastback. Al frente del A7 Sportback se ubica un 3.0L V6 TFSi con 333 caballos. La potencia es digerida por una caja S-tonic de doble embrague y administrada entre ambos ejes por el sistema quattro. La dotación de lujo, confort y seguridad está más que asegurada, así como la calidad Audi sumada a la opulencia propia de sus modelos grandes.',
+            descripcion: 'Una berlina del segmento del gran sedán de lujo.',
             fecha_de_carga: '01/04/2022',
         },{
             nombre: 'Audi A8',
             imagen: '/images/productos/Audi_A8.jpeg',
-            descripcion: 'El buque insignia de los cuatro aros tiene nueva generación, pero mantiene las cualidades que lo distinguen, desde el chasis realizado en aluminio hasta la tracción integral. Además se suma lujo, calidad, refinamiento y mucha tecnología como la posibilidad de una mecánica híbrida como la que probamos en exclusiva en México.',
+            descripcion: 'Una berlina de lujo, del segmento F.',
             fecha_de_carga: '01/05/2022',
+        },{
+            nombre: 'Audi e-tron',
+            imagen: '/images/productos/Audi_e-tron_Sportback.jpg',
+            descripcion: 'Automóvil 100% eléctrico con una autonomía que ofrece libertad.',
+            fecha_de_carga: '01/11/2022',
         },{
             nombre: 'Audi e-tron GT',
             imagen: '/images/productos/Audi_e-tron_GT.jpeg',
-            descripcion: 'Audi lanzó en Argentina su sedán 100% eléctrico y para no dejar dudas de su capacidad, llegó en variante RS, la más deportiva del momento. Mecánicamente ofrece: dos motores eléctricos, uno por eje lo que entrega tracción quattro®, 590 CV (636 CV disponibles por 2s5 con Launch Control), 830 Nm todo para un 0-100 km/h de 3s3. La batería es de 84 kWh, rinde hasta 455 km de autonomía y puede usar recarga ultra rápida con tecnología de 800 voltios.',
+            descripcion: 'Una berlina eléctrica con diseño coupé, del segmento E.',
             fecha_de_carga: '01/06/2022',
         },{
             nombre: 'Audi Q2',
             imagen: '/images/productos/Audi_Q2.jpeg',
-            descripcion: 'El benjamín de la familia Q se destaca por su espíritu juvenil, algo que se refleja en las medidas compactas y en el estilo más anguloso que el resto de la gama de Audi. El Q2 es chico pero tiene la potencia del 1.4 TSi de 150 CV asociado a caja S-Tronic (doble embrague) de 7 relaciones. Enfocado hacia el uso urbano, solo se ofrece con tracción simple.',
+            descripcion: 'Un SUV de tamaño pequeño, del segmento B.',
             fecha_de_carga: '01/07/2022',
         },{
             nombre: 'Audi Q3',
             imagen: '/images/productos/Audi_Q3.jpeg',
-            descripcion: 'Fue el SUV más chico y juvenil de Audi hasta la llegada del Q2. Se destaca por su comportamiento dinámico más enfocado hacia el uso en caminos y por la típica calidad interior de la marca. La gama mecánica incluye todos motores con turbo y posibilidad de tracción quattro.',
+            descripcion: 'Un SUV del segmento C.',
             fecha_de_carga: '01/08/2022',
         },{
             nombre: 'Audi Q5',
             imagen: '/images/productos/Audi_Q5.jpeg',
-            descripcion: 'La segunda generación del Audi Q5 cambió para seguir ofreciendo lo mismo, gran calidad, potencia y refinamiento premium. Si bien la gama es simple, ostenta mucha potencia con un 2.0L y un V6 3.0, ambos sobrealimentados. Completa tracción quattro y transmisiones automáticas de ultima generación con 7 u 8 cambios.',
+            descripcion: 'Un todocamino mediano, del segmento C.',
             fecha_de_carga: '01/09/2022',
         },{
             nombre: 'Audi Q7',
             imagen: '/images/productos/Audi_Q7.jpeg',
-            descripcion: 'Fue el primer integrante de la familia Q y si bien comparte plataforma con el Volkswagen Touareg es más grande, lo que le permite ofrecer tres filas de asientos. En esta, su segunda generación, el modelo es más dinámico gracias a un ahorro de peso que se une a dos 3.0L V6 sobrealimentados, el naftero con 333 CV y el diesel de 249 caballos. En ambos casos la caja es automática de 8 cambios y la tracción quattro. Está de más hablar de la dotación y calidad percibida a bordo de este SUV de Audi.',
+            descripcion: 'Un SUV premium de gran tamaño, del segmento E.',
             fecha_de_carga: '01/10/2022',
+        },{
+            nombre: 'Audi RS Q8',
+            imagen: '/images/productos/Audi_RS_Q8.jpg',
+            descripcion: 'Es un SUV coupé, del segmento E.',
+            fecha_de_carga: '01/12/2022',
         }
     ],
     usuario: {
