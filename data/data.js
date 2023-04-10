da0e2381828e9db3284fdd70bf653064d462fc08
nvillalonga <nvillalonga@udesa.edu.ar>
2023-04-10 09:55:02 -0300

    creacion de data

---
+++ data/data.js
@@ -0,0 +1,247 @@
+let data = {
+    productos: [
+        {
+            nombre: '',
+            imagen: '',
+            descripcion: '',
+            fecha_de_carga: '',
+            comentarios: [
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+            ]
+        },
+        {
+            nombre: '',
+            imagen: '',
+            descripcion: '',
+            fecha_de_carga: '',
+            comentarios: [
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+            ]
+        },{
+            nombre: '',
+            imagen: '',
+            descripcion: '',
+            fecha_de_carga: '',
+            comentarios: [
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+            ]
+        },{
+            nombre: '',
+            imagen: '',
+            descripcion: '',
+            fecha_de_carga: '',
+            comentarios: [
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+            ]
+        },{
+            nombre: '',
+            imagen: '',
+            descripcion: '',
+            fecha_de_carga: '',
+            comentarios: [
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+            ]
+        },{
+            nombre: '',
+            imagen: '',
+            descripcion: '',
+            fecha_de_carga: '',
+            comentarios: [
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+            ]
+        },{
+            nombre: '',
+            imagen: '',
+            descripcion: '',
+            fecha_de_carga: '',
+            comentarios: [
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+            ]
+        },{
+            nombre: '',
+            imagen: '',
+            descripcion: '',
+            fecha_de_carga: '',
+            comentarios: [
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+            ]
+        },{
+            nombre: '',
+            imagen: '',
+            descripcion: '',
+            fecha_de_carga: '',
+            comentarios: [
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+            ]
+        },{
+            nombre: '',
+            imagen: '',
+            descripcion: '',
+            fecha_de_carga: '',
+            comentarios: [
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+            ]
+        },{
+            nombre: '',
+            imagen: '',
+            descripcion: '',
+            fecha_de_carga: '',
+            comentarios: [
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+                {
+                    nombre_usuario: '',
+                    foto_perfil_usuario: '',
+                    comentario: ''
+                },
+            ]
+        },
+}
