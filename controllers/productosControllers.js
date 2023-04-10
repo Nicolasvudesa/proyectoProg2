e878d9e6235c5be24416292b2af9ffe018b64812
Lucioghidini <lghidini@udesa.edu.ar>
2023-04-10 12:17:55 -0300

    cambios

--- controllers/productosControllers.js
+++ controllers/productosControllers.js
@@ -1,7 +1,11 @@
 const controller = {
     index: function(req,res){
         res.send('aca mandamos los productos')
-    }
+    
+    },
+    add:  function(req,res){
+        res.render('product-add')
+}
 }
 
 module.exports = controller
