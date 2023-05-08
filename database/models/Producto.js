5072475b2ec721f50b6d934e24553cf59ed6322c
nvillalonga <nvillalonga@udesa.edu.ar>
2023-05-08 11:34:30 -0300

    correciones en models

--- database/models/Producto.js
+++ database/models/Producto.js
@@ -24,7 +24,8 @@ module.exports = function (sequelize, dataType){
     genre_id:{
         type: dataTypes.INTEGER,
     }};
-    let config = {tableName: 'productos',
+
+    let config = {tableName: 'tablaProductos',
     timestamps: false, //Si la tabla no tiene los campos created_at y updated_at
     underscored: true, //Si los nombres de las columnas en la db tienen guiones bajos en lugar de camelCase.,
     };
