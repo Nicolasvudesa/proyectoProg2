574c6cbbaccae0f635ff47e15d5bea3144f534a9
nvillalonga <nvillalonga@udesa.edu.ar>
2023-05-08 10:25:03 -0300

    Actualizacion

--- database/config/config.js
+++ database/config/config.js
@@ -8,15 +8,15 @@ module.exports = {
   },
   "test": {
     "username": "root",
-    "password": null,
-    "database": "database_test",
+    "password": 'hola123',
+    "database": "base_de_datos",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
-    "password": null,
-    "database": "database_production",
+    "password": 'hola123',
+    "database": "base_de_datos",
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
