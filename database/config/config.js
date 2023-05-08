6f6cd160a56d8ff18f1485d2fe9f12514c79d6a1
nvillalonga <nvillalonga@udesa.edu.ar>
2023-05-08 10:40:38 -0300

    Vuelta atras config

--- database/config/config.js
+++ database/config/config.js
@@ -8,15 +8,15 @@ module.exports = {
   },
   "test": {
     "username": "root",
-    "password": 'hola123',
-    "database": "base_de_datos",
+    "password": null,
+    "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
-    "password": 'hola123',
-    "database": "base_de_datos",
+    "password": null,
+    "database": "database_production",
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
