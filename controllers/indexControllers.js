39ce588e013b0727ce6c95b45ad77485aae618de
unknown <lghidini@udesa.edu.ar>
2023-06-07 19:10:52 -0300

    camb

--- controllers/indexControllers.js
+++ controllers/indexControllers.js
@@ -13,7 +13,41 @@ const indexController = {
         .catch(function(error){
             console.log(error)
         });
+    },
+    show: (req, res) => {
+        let id = req.params.id; 
+    
+        Producto.findByPk(id)
+        .then(function(result) {
+          return res.render('prodcut', {
+            productos: result
+          })
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    
+      },
+    
+      resultado: (req, res) =>{
+        let busqueda = req.query.product;
+    
+        Producto.findAll(
+          {
+            where: [
+              /* { title: busqueda} */
+             /*  {title: {[op.like]: `%${busqueda}%`}} */
+             {title: {[op.like]: '%' + busqueda + '%'}}
+            ]
+          }
+        ).then(function(result) {
+          return res.render('search-results', {/*lo que recibe la busqueda!! : result*/})
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+        },
     }
-}
-
 module.exports = indexController;
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
-const controller = require("../controllers/indexControllers")
+const indexController = require("../controllers/indexControllers")
 /* GET home page. */
-router.get('/', controller.index)
-
+router.get('/', indexController.index)
+router.get('/id/:id', indexController.show)
+router.get('/busqueda', indexController.resultado)
 module.exports = router;
