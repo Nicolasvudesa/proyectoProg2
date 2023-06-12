625161f89a57e93420604be2d71f415ded692526
unknown <lghidini@udesa.edu.ar>
2023-06-12 14:27:05 -0300

    mas cambios

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const controller = require("../controllers/usersControllers")
 
 /* GET users listing. */
 router.get('/login', controller.mostrarLogin)
-router.get('/loginUsuario', controller.login)
+router.get('/login', controller.login)
 router.get('/logout', controller.logout); 
 router.get('/registrate', controller.registro);
 router.post('/guardar', controller.guardar);
