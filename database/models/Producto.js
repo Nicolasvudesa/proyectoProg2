6ed4c18381dd30cbfbaebec204559474fa0a8f41
nvillalonga <nvillalonga@udesa.edu.ar>
2023-06-12 10:07:37 -0300

    Correcion models

--- database/models/Producto.js
+++ database/models/Producto.js
@@ -38,8 +38,8 @@ module.exports = function (sequelize, dataTypes){
 
     let config = {
         tableName: 'tablaProductos',
-        timestamps: false, //Si la tabla no tiene los campos created_at y updated_at
-        underscored: true, //Si los nombres de las columnas en la db tienen guiones bajos en lugar de camelCase.,
+        timestamps: true, //Si la tabla no tiene los campos created_at y updated_at
+        underscored: false, //Si los nombres de las columnas en la db tienen guiones bajos en lugar de camelCase.,
     };
 
     const Producto = sequelize.define(alias, cols, config);
