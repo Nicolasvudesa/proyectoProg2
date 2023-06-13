fa3d0c46c3144e4fb155b9e642016ad7d8d7ce4f
unknown <lghidini@udesa.edu.ar>
2023-06-13 15:23:28 -0300

    cambios

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,59 +1,37 @@
-const data = require("../data/data");
+const db = require("../database/models");
+const user = db.Usuario
+const bcrypt = require('bcryptjs');
 
 const controller = {
     mostrarLogin: function (req, res) {
         res.render('login');
     },
     login: function (req, res) {
-        let form = req.body
-        let usuario = form.usuario
-        let contraseña = form.contra
+        let emailBuscado = req.body.usuario
+        let contraseña = req.body.contra
         let filtrado = {
-            where: [{ usuario: usuario }]
+            where: [{ usuario: emailBuscado }]
         }
 
-        db.Usuario.findOne(filtrado)
-        .then(function (result) {
-            let error = {};
-            if (email == '') {
-                error.message = "El mail está vacío."
-                res.locals.error = error;
-                return res.render('login')
-            }
-            if (result == null) {
-                error.message = "Email no encontrado"
-                res.locals.error = error;
-                return res.render('login')
-            }
-            if (contra == '') {
-                error.message = "Contraseña es un campo obligatorio"
-                res.locals.error = error;
-                return res.render('login')
-            }
-        
-            let contraseñaEncriptada = result.contraseña
-            let check = bcrypt.compareSync(contraseña, contraseñaEncriptada)
-
-            if  (check == true) {
-
-                req.session.user = result
-
-                if (req.body.recordame != undefined) {
-                    res.cookie('userId', result.id, { maxAge: 1000 * 60 * 15000 });
-                }
-                
-                return res.redirect('/products/all')
-
-            } else {
-                error.message = "Contraseña incorrecta."
-                res.locals.error = error;
-                return res.render('login')
-            }
-
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
+        user.findOne(filtrado)
+        .then((result) => {
+          
+            if (result != null) {
+                      let claveCorrecta = bcrypt.compareSync(contraseña, result.contra)
+                      if (claveCorrecta) {
+                          req.session.user = result.dataValues;
+                          return res.redirect('/all');
+  
+                      } else {
+                          return res.send("La contraseña ingresada es incorrecta");
+                      }
+                  } else {
+                      return res.send("Este mail es inexistente")
+                  }
+  
+              }).catch((err) => {
+                  console.log(err);
+              });
 
 },
         logout: function (req, res) {
